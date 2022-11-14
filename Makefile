14742b197b9c2c72be3d7849999f2a2885f90386
Rahat <rahat.muneeb@hatless.studio>
2022-11-14 14:00:35 +0000

    changes to make cachebw static

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
-COMPILER=NVCC
-ARCH=sm_60
+COMPILER=GNU
+ARCH=armv8.4-a
 SHMEM=0
 
-COMPILER_GNU=gcc
+COMPILER_GNU=aarch64-linux-gnu-gcc
 COMPILER_INTEL=icc
 COMPILER_NVCC=nvcc
 CC=$(COMPILER_$(COMPILER))
@@ -11,6 +11,8 @@ CFLAGS_INTEL=-std=c99 -x$(ARCH) -qopt-zmm-usage=high -qopenmp
 CFLAGS_GNU=-std=c11 -march=$(ARCH) -fopenmp
 CFLAGS_NVCC=-arch=$(ARCH) -DGPU -x cu -DSHMEM=$(SHMEM)
 
+GNU_STATIC_FLAGS=-std=c11 -O3 -fPIC -I/usr/lib/gcc-cross/aarch64-linux-gnu/11/include -static -static-libgcc -static-libstdc++
+STATIC_OMP=/usr/lib/gcc-cross/aarch64-linux-gnu/11/libgomp.a
 CFLAGS = -O3 $(CFLAGS_$(COMPILER))
 
 HEADERS = $(wildcard *.h)
@@ -19,6 +21,15 @@ default: triad
 
 triad: cachebw.c $(HEADERS)
 	$(CC) $(CFLAGS) cachebw.c -o cachebw
+
+triad_static_simeng: cachebw.c $(HEADERS)
+	aarch64-linux-gnu-gcc $(GNU_STATIC_FLAGS) -march=armv8.4-a cachebw.c $(STATIC_OMP) -o cachebw_static_pmu
+
+triad_static_sve_simeng: cachebw.c $(HEADERS)
+	aarch64-linux-gnu-gcc $(GNU_STATIC_FLAGS) -march=armv8.4-a+sve cachebw.c $(STATIC_OMP) -o cachebw_static_sve
+
+triad_x86_pmu: cachebw.c $(HEADERS)
+	gcc -std=c11 -O3 -fPIC -I/usr/lib/gcc/x86_64-linux-gnu/11/include -static -static-libgcc -static-libstdc++ cachebw.c /usr/lib/gcc/x86_64-linux-gnu/11/libgomp.a -o cachebw_static_pmu
 
 clean:
 	-rm -rf cachebw
--- cachebw.c
+++ cachebw.c
@@ -1,26 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include "triad.h"
 
-int main(int argc, char** argv)
-{
+#include "pmu.h"
+
+int main(int argc, char **argv) {
   if (argc < 3) {
     fprintf(
         stderr,
         "error: need 2 input args; (1) Size of data per thread(CPU)/SM(GPU) in KiB, (2) Number of reps\n");
     return 1;
-  }
-
+  };
   int kbytes = atoi(argv[1]);
   int nreps = atoi(argv[2]);
-
+  int runs = atoi(argv[3]);
+  RUN_PERF(
   size_t n = ((size_t)kbytes * 1024) / (3 * sizeof(double));
-
   double tot_mem_bw = cache_triad(n, nreps);
-
-  printf("n %8zu, reps = %8d, bytes %12zu, bandwidth = %12f\n", n, nreps,
-         n * 3 * sizeof(double), tot_mem_bw);
-
+  // printf("[SSTSimEng:SSTDebug] OutputLine-n %zu, reps = %d, bytes %zu, bandwidth = %f\n", n, nreps,
+  //        n * 3 * sizeof(double), tot_mem_bw);
+  );
   return 0;
 }
+
+
+    // Perf setup
+  // uint64_t cycle_count = 0;
+  // int grp_fd;
+  // uint64_t cyc_id;
+
+  // grp_fd = create_perf_event(0x011, -1);
+  // cyc_id = get_perf_event_id(grp_fd);
+
+  // // Perf counting
+  // reset_perf_event(grp_fd, 1);
+  // enable_perf_event(grp_fd, 1);
+
+    // Perf closing
+  // disable_perf_event(grp_fd, 1);
+  // cycle_count = read_perf_event(grp_fd, cyc_id, 1);
+  // printf("%ld-%ld\n",0x11, cycle_count);
+
+
